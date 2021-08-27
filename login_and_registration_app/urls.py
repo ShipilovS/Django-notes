9ab5d8e90c17755f5d07836e81f9b1862ab56443
ShipilovS <stefan.shipilov@mail.ru>
2021-08-27 20:19:10 +0300

    create a new app for login and registration

---
+++ login_and_registration_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('login/', login_user, name='login'),
+    path('registration/', registration_user, name='registration'),
+    path('loguot/', logout_user, name='loguot'),
+]
